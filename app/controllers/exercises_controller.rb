08dc209b17ea5c483d5a394e2cb4e6f612e93e81
kasagata <k.sagata.1207@gmail.com>
2024-02-09 15:50:06 +0900

    exercise1_2

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,23 +6,23 @@ class ExercisesController < ApplicationController
   end
 
   def exercise2
-    # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
-    #   * left_outer_joinsを使うこと
-    @foods = left_outer_joins(:order_foods).where(order_foods: {food_id: nil })
-    @shops = Shop.left_outer_joins(:foods).@food
+    # # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
+    # #   * left_outer_joinsを使うこと
+    # @foods = left_outer_joins(:order_foods).where(order_foods: {food_id: nil })
+    # @shops = Shop.left_outer_joins(:foods).@food
   end
 
   def exercise3 
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address
+    # @address = Address
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
+    # @customer = Customer
   end
 end
