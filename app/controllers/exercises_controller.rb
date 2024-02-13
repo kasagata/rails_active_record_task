323bee12269bd7d9e7c4708da965776673e14014
kasagata <k.sagata.1207@gmail.com>
2024-02-13 15:11:07 +0900

    課題提出前チェック5

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,7 +8,7 @@ class ExercisesController < ApplicationController
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop.left_outer_joins(foods: :order_foods).where(order_foods: {food_id: nil })
+    @shops = Shop.left_outer_joins(foods: :order_foods).where(order_foods: {food_id: nil }).distinct
     # SELECT "shops".* FROM "shops" 
     # LEFT OUTER JOIN "foods" 
     # ON "foods"."shop_id" = "shops"."id" 
@@ -42,7 +42,7 @@ class ExercisesController < ApplicationController
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer.joins(orders: :foods).group(:name).order('sum_price DESC').sum(:price).first
+    @customer = Customer.joins(orders: :foods).group(:name).order('sum_price DESC').sum(:price)
     
   end
 end
